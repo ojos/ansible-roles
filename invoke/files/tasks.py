53adbdda9d983f1dabab9e5887e3735f880bb7d4
Tsuyoshi Aizu <aizu@bascule.co.jp>
2016-01-20 12:12:39 +0900

    added orchestration role

--- invoke/files/tasks.py
+++ invoke/files/tasks.py
@@ -15,3 +15,34 @@ def Hello():
 def subl():
     """Sublime Textでプロジェクトを開く"""
     run('subl %s' % os.environ['PROJECT_HOME'])
+
+
+@task
+def atom():
+    """Atomでプロジェクトを開く"""
+    run('atom %s' % os.environ['PROJECT_HOME'])
+
+
+@task
+def restart():
+    """サーバーを再起動する"""
+    pass
+
+
+@task
+def deploy(branch='master'):
+    """デプロイ&再起動する"""
+    pass
+
+
+@task
+def unittest(options=''):
+    """アプリケーションサーバーをテストする"""
+    pass
+
+
+@task
+def ssh(host="vagrant", env='vagrant'):
+    """sshラッパー"""
+    run('ssh -F %(dir)s/ssh_config -i %(dir)s/%(env)s.pem %(host)s' %
+        {'dir': os.environ['KEY_DIR'], 'env': env, 'host': host})
