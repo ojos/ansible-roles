6b690553388e73efa3d871531656a8756d40c199
Tsuyoshi Aizu <aizu@bascule.co.jp>
2016-05-26 21:13:29 +0900

    fixed kcl_python role

--- kcl_python/files/kcl_app.py
+++ kcl_python/files/kcl_app.py
@@ -14,7 +14,7 @@ logger = getLogger(__name__)
 logger.setLevel(DEBUG)
 formatter = Formatter('%(asctime)s - %(levelname)s - %(message)s')
 handler = RotatingFileHandler(
-    filename='../../log/kcl/python.log',
+    filename='../../log/kcl/sub.log',
     maxBytes=1024 * 1024 * 100,
     backupCount=5
 )
